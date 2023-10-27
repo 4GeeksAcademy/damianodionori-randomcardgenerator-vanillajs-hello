df7148b452323ad539a9208aa7d7ec612ee24940
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-10-27 23:01:12 +0000

    random card generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,56 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateRandomCard();
 };
+
+function generateRandomCard() {
+  const suits = ["hearts", "diamonds", "spades", "clubs"];
+  const cardValues = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  const randomSuitIndex = Math.floor(Math.random() * suits.length);
+  const randomValueIndex = Math.floor(Math.random() * cardValues.length);
+
+  const cardDiv = document.getElementById("card");
+  cardDiv.classList = "card " + suits[randomSuitIndex];
+
+  const suitSymbol = getSuitSymbol(suits[randomSuitIndex]);
+  const cardValue = cardValues[randomValueIndex];
+
+  const topLeft = cardDiv.querySelector(".top-left");
+  const bottomRight = cardDiv.querySelector(".bottom-right");
+  const value = cardDiv.querySelector(".value");
+
+  topLeft.textContent = suitSymbol;
+  bottomRight.textContent = suitSymbol;
+  value.textContent = cardValue;
+}
+
+function getSuitSymbol(suit) {
+  switch (suit) {
+    case "hearts":
+      return "♥";
+    case "diamonds":
+      return "♦";
+    case "spades":
+      return "♠";
+    case "clubs":
+      return "♣";
+    default:
+      return "";
+  }
+}
